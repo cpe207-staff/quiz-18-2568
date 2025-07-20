326560a36ad7687ec53626f0aef4d852bc73790f
potikanond <potikanond@gmail.com>
2025-07-20 10:14:24 +0700

    update questions

--- q5.ts
+++ q5.ts
@@ -1,12 +1,16 @@
-/* define interface for Mafia objects */
+/* define interface for Yakusa objects */
+interface Yakusa {
+  id: number;
+  power: number;
+}
 
 /* assign interface/type to the function definition properly */
-const q5 = (mafias, fights) => {
+const q5 = (yakusas, fights) => {
   /* Your code here */
 };
 
 //Test case
-const mafias = [
+const yakusas = [
   { id: 1, power: 5 },
   { id: 2, power: 30 },
   { id: 3, power: 60 },
@@ -14,6 +18,6 @@ const mafias = [
   { id: 5, power: 70 },
 ];
 const fights = [5, 2, 4, 3, 1, 3];
-console.log(q5(mafias, fights));
+console.log(q5(yakusas, fights));
 
 export default q5;
