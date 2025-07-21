6a4209bbabd0de3641e45264e1733b385428e169
D. Potikanond <potikanond@gmail.com>
2025-07-21 10:23:54 +0700

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -4,3 +4,14 @@ export default {
     "@babel/preset-typescript",
   ],
 };
+
+// สำหรับคนที่ใช้คำสั่ง npm test ... แล้ว module error
+// 1. ให้ลองเปลี่ยนชื่อไฟล์ "babel.config.js" เป็น "babel.config.cjs"
+// 2. ให้ลองใช้ code block ด้านล่างนี้แทน
+
+// module.exports = {
+//   presets: [
+//     ["@babel/preset-env", { targets: { node: "current" } }],
+//     "@babel/preset-typescript",
+//   ],
+// };
